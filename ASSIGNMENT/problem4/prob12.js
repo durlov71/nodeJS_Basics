002ffc57f7284273bfbe1f506f9bc72fd61eccd1
Dark <istiak.csinfo@gmail.com>
2022-05-01 23:14:46 +0600

    new

--- ASSIGNMENT/problem4/prob12.js
+++ ASSIGNMENT/problem4/prob12.js
@@ -1,7 +1,9 @@
 // Assignment 12
+
 const { v4: uuidv4 } = require('uuid');
 console.log(uuidv4());
+
 const chalk = require('chalk');
 console.log(chalk.green('Hello world'));
-console.log(chalk.blue.bgRed.bold('Hello world!'));
-console.log(chalk.underline.bgRed('Hello world'));
+console.log(chalk.blue.bold('Hello world!'));
+console.log(chalk.underline.bgYellow('Hello world'));
